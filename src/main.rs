7bb767c4f1758404d6a33a3639ab93a2ba70ac4b
Rik de Kort <rik.de.kort@pggm.nl>
2020-02-16 11:44:48 +0100

    Rng as mutable borrow

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ use std::error::Error;
 use mcmc::output;
 
 
-fn next<R: Rng>(x: f64, pi: fn(f64) -> f64, proposal: Normal<f64>, mut rng: R) -> f64 {
-    let candidate = x + proposal.sample(&mut rng);
+fn next<R: Rng>(x: f64, pi: fn(f64) -> f64, proposal: Normal<f64>, rng: &mut R) -> f64 {
+    let candidate = x + proposal.sample(rng);
 
     let alpha = (pi(candidate) / pi(x)).min(1.0);
-    let u: f64 = rng.gen();
+    let u: f64 = rng.gen();  // Draws uniform [0, 1)
     if u <= alpha {
         return candidate;
     } else {
@@ -20,24 +20,23 @@ fn next<R: Rng>(x: f64, pi: fn(f64) -> f64, proposal: Normal<f64>, mut rng: R) -
     }
 }
 
-fn metropolis<R: Rng>(pi: fn(f64) -> f64, proposal: Normal<f64>, mut rng: R) -> Vec<f64> {
-    // let rng = rand::thread_rng();
-    let local_next = |x: f64| { next(x, pi, proposal, rng) };
+fn metropolis<R: Rng>(pi: fn(f64) -> f64, proposal: Normal<f64>, rng: &mut R) -> Vec<f64> {
+    let local_next = |x: f64, rng: &mut R| { next(x, pi, proposal, rng) };
 
     // Execute warmup
-    let n_warmup = 10000;
+    let n_warmup = 1e5 as usize;
     let mut x = 10.0;
     for _ in 1..n_warmup{
-        x = local_next(x);
+        x = local_next(x, rng);
     }
 
     // Start running the simulation
-    let n = 1_000_000;
+    let n = 1e6 as usize;
     let mut result = Vec::with_capacity(n); 
     result.push(x);
 
     for i in 1..n {
-        let next_val = local_next(result[i-1]);
+        let next_val = local_next(result[i-1], rng);
         result.push(next_val);
     }
     return result;
@@ -53,7 +52,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // exp( -x ^ 2 ) is the distribution propertion
     let pi = |x: f64| -> f64 { (-x.powi(2)).exp() };
 
-    let result = metropolis(pi, proposal, rng);
+    let result = metropolis(pi, proposal, &mut rng);
 
     output::write_vec_to_csv(result)
 }
