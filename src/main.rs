eef6ca580ac08fe51645d9d532e2236bc637ed9b
Rik de Kort <rik.de.kort@pggm.nl>
2020-02-16 12:01:09 +0100

    Move alg to own file

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,8 @@
-extern crate csv; 
-
-use rand::Rng;
-use rand::distributions::{Distribution};
 use ::rand_distr::Normal;
-
 use std::error::Error;
+
 use mcmc::output;
-
-
-fn next<R: Rng>(x: f64, pi: fn(f64) -> f64, proposal: Normal<f64>, rng: &mut R) -> f64 {
-    let candidate = x + proposal.sample(rng);
-
-    let alpha = (pi(candidate) / pi(x)).min(1.0);
-    let u: f64 = rng.gen();  // Draws uniform [0, 1)
-    if u <= alpha {
-        return candidate;
-    } else {
-        return x;
-    }
-}
-
-fn metropolis<R: Rng>(pi: fn(f64) -> f64, proposal: Normal<f64>, rng: &mut R) -> Vec<f64> {
-    let local_next = |x: f64, rng: &mut R| { next(x, pi, proposal, rng) };
-
-    // Execute warmup
-    let n_warmup = 1e5 as usize;
-    let mut x = 10.0;
-    for _ in 1..n_warmup{
-        x = local_next(x, rng);
-    }
-
-    // Start running the simulation
-    let n = 1e6 as usize;
-    let mut result = Vec::with_capacity(n); 
-    result.push(x);
-
-    for i in 1..n {
-        let next_val = local_next(result[i-1], rng);
-        result.push(next_val);
-    }
-    return result;
-}
-
+use mcmc::metropolis::metropolis;
 
 
 fn main() -> Result<(), Box<dyn Error>> {
