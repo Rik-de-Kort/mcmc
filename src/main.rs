68479c03b41113a7e3884bce3a23c1d1a22ccdc8
Rik de Kort <rik.de.kort@pggm.nl>
2020-02-21 22:08:06 +0100

    away with this plotting shenanigans!

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ use mcmc::output;
 use mcmc::metropolis::metropolis;
 use mcmc::quality_of_life::*;
 
-use plotters::prelude::*;
-
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut rng = rand::thread_rng();
@@ -18,81 +16,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let result = metropolis(pi, &proposal, &mut rng);
 
-    let n_buckets = 5000;
-    let min = partial_min(&result);
-    let max = partial_max(&result);
-    let bucket_size = (max - min) / n_buckets as f32;
-    
-    let mut histogram = [0; 5001];
-
-    for x in result.iter() {
-        // min + i*bucket size <= x < min + (i+1) * bucket_size
-        // therefore i <= (x - min) / bucket_size
-        let i = ((x - min) / bucket_size).floor() as usize;
-
-        // In case x = max, put in highest bucket.
-        let i = if i == 5000 { 5000 - 1 } else { i };
-        histogram[i] = histogram[i] + 1;
-    }
-
-
-
-    // let root = BitMapBackend::new("histogram.png", (640, 480)).into_drawing_area();
-    // root.fill(&WHITE)?;
-
-    // let mut chart = ChartBuilder::on(&root)
-    //     .x_label_area_size(35)
-    //     .y_label_area_size(40)
-    //     .margin(5)
-    //     .caption("Histogram Test", ("sans-serif", 50.0).into_font())
-    //     .build_ranged(0u32..5000u32, 0u32..1000u32)?;
-
-    // chart.configure_mesh().draw()?;
-
-    // chart.draw_series(
-    //     Histogram::vertical(&chart)
-    //     .data(histogram.iter().map(|x: &u32| (*x, 1)))
-    //     )?;
-
-    // println!("min: {}, max: {}", min, max);
-    // println!("maxitems: {}", partial_max(&histogram.to_vec()));
-
-    let root = BitMapBackend::new("histogram.png", (640, 480)).into_drawing_area();
-
-    root.fill(&WHITE)?;
-
-    let mut chart = ChartBuilder::on(&root)
-        .x_label_area_size(35)
-        .y_label_area_size(40)
-        .margin(5)
-        .caption("Histogram Test", ("sans-serif", 50.0).into_font())
-        .build_ranged(0u32..5000u32, 0u32..1000u32)?;
-
-    chart
-        .configure_mesh()
-        .disable_x_mesh()
-        .line_style_1(&WHITE.mix(0.3))
-        .x_label_offset(30)
-        .y_desc("Count")
-        .x_desc("Bucket")
-        .axis_desc_style(("sans-serif", 15).into_font())
-        .draw()?;
-
-
-    chart.draw_series(
-        Histogram::vertical(&chart)
-            .style(RED.mix(0.5).filled())
-            .data(histogram.iter().map(|x: &u32| (*x, 1))),
-    )?;
-
-    
-    // output::write_vec_to_csv(result)
-    let mut res = Vec::new();
-    for x in histogram.iter(){
-        res.push(*x as f32);
-    }
-    output::write_vec_to_csv(res)
-
+    let hist = output::get_hist(result, 500);
+    output::write_vec_to_csv(hist)
 }
 
 
--- src/output.rs
+++ src/output.rs
@@ -4,6 +4,9 @@ use std::string::ToString;
 use std::error::Error;
 use csv::Writer;
 
+use crate::quality_of_life::*;
+
+
 pub fn write_vec_to_csv<F: Float+ToString>(result: Vec<F>) -> Result<(), Box<dyn Error>> {
     let mut wtr = Box::new(Writer::from_path("data.csv")?);
     wtr.write_record(&["index", "val"])?;
@@ -13,4 +16,24 @@ pub fn write_vec_to_csv<F: Float+ToString>(result: Vec<F>) -> Result<(), Box<dyn
     }
     wtr.flush()?;
     Ok(())
+}
+
+pub fn get_hist(data: Vec<f32>, n_buckets: usize) -> Vec<f32> {
+    let min = partial_min(&data);
+    let max = partial_max(&data);
+    let bucket_size = (max - min) / n_buckets as f32;
+    
+    // let mut histogram = Vec::with_capacity(n_buckets);
+    let mut histogram = vec![0.0; n_buckets];
+
+    for x in data.iter() {
+        // min + i*bucket size <= x < min + (i+1) * bucket_size
+        // therefore i <= (x - min) / bucket_size
+        let i = ((x - min) / bucket_size).floor() as usize;
+
+        // In case x = max, put in highest bucket.
+        let i = if i == 500 { 500 - 1 } else { i };
+        histogram[i] = histogram[i] + 1.0;
+    }
+    histogram
 }
